d0bc4eb216a498d63bced661966b695c323a8b8e
Borislav Ivanov <bsivanov@users.noreply.github.com>
2021-06-25 15:15:08 +0300

    Add sample code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as monaco from "monaco-editor";
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  template:`
+  <ng-monaco-editor 
+  style="height: 300px" 
+  [modelUri]="modelUri"
+  [(ngModel)]="jsonCode" 
+  [options]="options"
+  (editorChange)="editorChange($event)"
+  ></ng-monaco-editor>`,
 })
 export class AppComponent {
-  title = 'test-ng-monaco-editor';
+    jsonCode = [
+    '{',
+    '    "p1": "v3",',
+    '    "p2": false',
+    "}"
+  ].join('\n');
+
+  options = {
+    language: "json"
+  };
+
+  modelUri = "a://b/foo.json";
+
+  editorChange(editor: monaco.editor.IStandaloneCodeEditor): void {
+    monaco.languages.json.jsonDefaults.setDiagnosticsOptions({
+      validate: true,
+      schemas: [{
+        uri: "http://myserver/foo-schema.json",
+        fileMatch: [this.modelUri],
+        schema: {
+          type: "object",
+          properties: {
+            p1: {
+              enum: ["v1", "v2"]
+            },
+            p2: {
+              enum: ["x1", "x2"]
+            }
+          }
+        }
+      }],
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { MonacoEditorModule } from 'ng-monaco-editor';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +10,27 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, 
+    FormsModule,
+    MonacoEditorModule.forRoot({
+      /**
+       * optional, load monaco by yourself, you'd prefer loading esm for example
+       */
+      //dynamicImport: () => import('monaco-editor'),
+
+      /**
+       * optional, use amd loader to load monaco if present, lower priority than `dynamicImport`
+       *
+       * Angular CLI currently does not handle assets with hashes. We manage it by manually adding
+       * version numbers to force library updates:
+       */
+      baseUrl: 'lib',
+
+      defaultOptions: {},
+    }),
+  ],
+  exports: [
+    MonacoEditorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
